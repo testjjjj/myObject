b85f9a490020bf34e2a00b267ffb3c5c36be17f6
cuijindong <1019338285@qq.com>
2019-12-31 17:36:27 +0800

    sdfs

--- src/views/write/index.vue
+++ src/views/write/index.vue
@@ -4,7 +4,16 @@
             <button @click="test">发表文章</button>
         </div>
         <div class="wr-main"></div>
-        <mavon-editor v-model="value" ref=md @imgAdd="imgAdd" heigth="100%" class="mavon"/> 
+        <mavon-editor 
+            toolbarsBackground="#dcdbdb"
+            previewBackground="#ffffff"
+            placeholder="写点啥老弟。。。"
+            codeStyle="atelier-lakeside-dark"
+            v-model="value"
+            ref=md
+            @imgAdd="imgAdd"
+            @change="mdChange"
+            class="mavon"/> 
     </div>
 </template>
 
@@ -13,12 +22,16 @@ import axios from 'axios'
 export default {
     data() {
         return {
-            value: ''
+            value: '<p><h3>js原型</h3><ul><ul><li><a href="#_1">什么是原型</a></li></ul></ul></p><h2><a id="_1"></a>什么是原型</h2>'
         }
     },
     methods: {
         test() {
             console.log(this.value)
+        },
+        mdChange(value, render) {
+            console.log(value)
+            console.log((render))
         },
         // 绑定@imgAdd event
         imgAdd(pos, $file){
